6ec45822b7ba0140fe509ae1b78a82b311191adf
Iyan-Core <awianton2@gmail.com>
2026-06-25 10:18:37 +0700

    Update homePage.tsx

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import Header from '../components/layout/Header';
+import Header from '../components/layout/header';
 
 export default function HomePage() {
   const navigate = useNavigate();
