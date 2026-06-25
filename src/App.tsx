84aac65e89f1117e069ee7b5137c5be230355330
Iyan-Core <awianton2@gmail.com>
2026-06-25 12:27:08 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import ReaderPage from './pages/ReaderPage';
+import HomePage from './pages/homePage';
+import ReaderPage from './pages/readerPage';
 
 function App() {
   return (
     <BrowserRouter basename="/Katalog-Book-apk/"> {/* sesuaikan dengan base di vite.config */}
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/reader" element={<ReaderPage />} />
+        <Route path="/" element={<homePage />} />
+        <Route path="/reader" element={<readerPage />} />
       </Routes>
     </BrowserRouter>
   );
