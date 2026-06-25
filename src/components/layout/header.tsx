3c1cb190c0da33ec09a0906660fdb0d75e94db04
Iyan-Core <awianton2@gmail.com>
2026-06-25 10:12:44 +0700

    Create header.tsx

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header style={{ padding: '1rem', background: '#1e293b', color: 'white', textAlign: 'center' }}>
+      <h1>📖 Katalog Buku 3D</h1>
+    </header>
+  );
+}
